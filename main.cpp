6267771fee4bec108ce13edae6b94765056afc14
Roman_Bychkov <bychkovr51@mail.ru>
2024-12-10 14:49:45 +0300

    Дружественные классы. Позволяют методам дружественного класса воздейтвовать на переменные класса

--- main.cpp
+++ main.cpp
@@ -1,58 +1,49 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <string>
-//Параграф дружественные функции для двух разных классов. Нужны нам, когда мы хотим
-//использовать в какой-либо функции две переменные разных классов
+//Параграф дружественные классы. Подобная вещь позволет нам сделать так, чтобы методы класса В могли обращаться к переменным класса А.
 
 using namespace std;
 
-class Car; //формальная декларация названия Car, нужна чтобы в 23 строке компилятор не удивился слову Car, так как класс Car
-class Person{   //будет объявлен только в 26 строки
+class MainHeroe; //формальная декларация класса MainHeroe, чтобы компилятор не ругался в 14 строке, так как MainHeroe будет объявлено
+class Vrag{      //только в 17 строке
 private:
-    string name;
-    string second_name;
-    int age;
+    int health = 100;
 
 public:
-    Person(string name, string second_name, int age){
-        this->name = name;
-        this->second_name = second_name;
-        this->age = age;
-    }
-
-    friend void print(Person& name, Person& second_name, Person& age, Car& marka); //friend - ключевое слово, для объявления
-};                                                                             //дружественной функции
-
-class Car{
-private:
-    string marka;
-    int horse_power;
-
-public:
-    friend void print(Person& name, Person& second_name, Person& age, Car& marka); //во втором классе также нужно объявить
-    Car(string marka, int horse_power){                                            //дружественную функцию через ключевое слово friend
-        this->marka = marka;
-        this->horse_power = horse_power;
-    }
+    friend class MainHeroe;    //объявление класса MainHeroe дружественным для класса Vrag
 };
 
+class MainHeroe{
+public:
+    void damage(Vrag& vrag){
+        vrag.health -=20;
+        cout << vrag.health << endl;
+    }
 
-void print(Person& name, Person& second_name, Person& age, Car& marka){//код дружественной функции
-    cout << "Имя владельца: " << name.name << " Фамилия владельца: " << second_name.second_name<< " Возраст: "\
-         << age.age << " Марка авто: " << marka.marka << endl;
-}
+    void heal(Vrag& vrag){
+        vrag.health += 20;
+        cout << vrag.health << endl;
+    }
 
-
+    void kill(Vrag& vrag){     //принимает объект класса Vrag
+        vrag.health = 0;
+        cout << vrag.health << endl;
+    }
+};
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "RU");
     QCoreApplication a(argc, argv);
 
-    Person name("Рома", "Бычков", 25);
-    Car marka("BMW", 249);
+    Vrag anybody;
+    MainHeroe Im;
 
-    print(name, name, name, marka);
+    Im.damage(anybody);
+
+    Im.kill(anybody);
+
 
     return 0;
 }
