a685bc83470e82ff1457ab3f39a4da788cb7a340
Roman_Bychkov <bychkovr51@mail.ru>
2024-12-10 11:15:36 +0300

    Обрати внимание на указатель this в классе Rabotnik

--- main.cpp
+++ main.cpp
@@ -3,31 +3,74 @@
 #include <string>
 
 
+using namespace std;
 
+class Rabotnik{
+private:
+    string name;
+    int year;
+    string second_name;
+
+public:
+    Rabotnik(string name, int year, string second_name){
+        this->name = name; //в данном случае this как бы конкретизирует, что мы обращаемся именно к переменной и именно данного класса
+        this->year = year;
+        this->second_name = second_name;
+    }
+
+    void set_name(string name){
+        this->name = name; //в данном случае this как бы конкретизирует, что мы обращаемся именно к переменной и именно данного класса
+    }
+    void set_year(int year){
+        this->year = year;
+    }
+
+    void set_second_name(int second_name){
+        this->second_name = second_name;
+    }
+
+    void get_info(){
+        cout << "Имя: " << name << endl;
+        cout << "Фамилия: " << second_name << endl;
+        cout << "Возраст: " << year << endl;
+    }
+};
 
 class Building{
 private:
     int year;
-    std::string type;
+    string type;
 public:
 
     //Building(){} //пустой конструктор
 
-    Building(int y, std::string t){ //конструктор инициализатор
+    Building(int y, string t){ //конструктор инициализатор
         //year = y;//чтобы эти данные не повторялись с функцией set_data, разумнее подставить сюда саму функцию set_data
         //type =t;
 
         set_data(y, t);
     }
 
-    void set_data(int y, std::string t){
+    Building(){} //пример перегрузки конструктора
+
+    Building(int y){ //второй пример перегрузки конструтора. При создании объекта класса, мы можем выбрать любой конструктор
+        year = y;
+        cout << "Ты воспользовался другим конструтором" << endl;
+    }
+
+    ~Building(){
+        cout << "Объект удален" << endl;
+    }
+
+    void set_data(int y, string t){
         year = y;
         type = t;
     }
 
     void get_info(){
-        std::cout << "Тип: " << type << " Год постройки: " << year << std::endl;
+        cout << "Тип: " << type << " Год постройки: " << year << endl;
     }
+
 
 };
 
@@ -66,6 +109,13 @@ int main(int argc, char *argv[])
     Building house(1991, "Жилой дом");
     school.get_info();
     house.get_info();
+    Building hospital(1982);
+
+    Rabotnik enjiener("Рома", 25, "Бычков");
+    enjiener.get_info();
+    enjiener.set_name("Вася");
+    enjiener.get_info();
+
 
     return 0;
 }
