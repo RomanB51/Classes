75b77c85a3532a7405f8f6c5a59936988ef1828c
Roman_Bychkov <bychkovr51@mail.ru>
2024-12-10 14:17:23 +0300

    Дружественные функции - это функции имеющие доступ к переменным обоих класов

--- main.cpp
+++ main.cpp
@@ -1,121 +1,58 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <string>
-
+//Параграф дружественные функции для двух разных классов. Нужны нам, когда мы хотим
+//использовать в какой-либо функции две переменные разных классов
 
 using namespace std;
 
-class Rabotnik{
+class Car; //формальная декларация названия Car, нужна чтобы в 23 строке компилятор не удивился слову Car, так как класс Car
+class Person{   //будет объявлен только в 26 строки
 private:
     string name;
-    int year;
     string second_name;
+    int age;
 
 public:
-    Rabotnik(string name, int year, string second_name){
-        this->name = name; //в данном случае this как бы конкретизирует, что мы обращаемся именно к переменной и именно данного класса
-        this->year = year;
+    Person(string name, string second_name, int age){
+        this->name = name;
         this->second_name = second_name;
+        this->age = age;
     }
 
-    void set_name(string name){
-        this->name = name; //в данном случае this как бы конкретизирует, что мы обращаемся именно к переменной и именно данного класса
-    }
-    void set_year(int year){
-        this->year = year;
-    }
+    friend void print(Person& name, Person& second_name, Person& age, Car& marka); //friend - ключевое слово, для объявления
+};                                                                             //дружественной функции
 
-    void set_second_name(int second_name){
-        this->second_name = second_name;
-    }
-
-    void get_info(){
-        cout << "Имя: " << name << endl;
-        cout << "Фамилия: " << second_name << endl;
-        cout << "Возраст: " << year << endl;
-    }
-};
-
-class Building{
+class Car{
 private:
-    int year;
-    string type;
+    string marka;
+    int horse_power;
+
 public:
-
-    //Building(){} //пустой конструктор
-
-    Building(int y, string t){ //конструктор инициализатор
-        //year = y;//чтобы эти данные не повторялись с функцией set_data, разумнее подставить сюда саму функцию set_data
-        //type =t;
-
-        set_data(y, t);
+    friend void print(Person& name, Person& second_name, Person& age, Car& marka); //во втором классе также нужно объявить
+    Car(string marka, int horse_power){                                            //дружественную функцию через ключевое слово friend
+        this->marka = marka;
+        this->horse_power = horse_power;
     }
-
-    Building(){} //пример перегрузки конструктора
-
-    Building(int y){ //второй пример перегрузки конструтора. При создании объекта класса, мы можем выбрать любой конструктор
-        year = y;
-        cout << "Ты воспользовался другим конструтором" << endl;
-    }
-
-    ~Building(){
-        cout << "Объект удален" << endl;
-    }
-
-    void set_data(int y, string t){
-        year = y;
-        type = t;
-    }
-
-    void get_info(){
-        cout << "Тип: " << type << " Год постройки: " << year << endl;
-    }
-
-
 };
+
+
+void print(Person& name, Person& second_name, Person& age, Car& marka){//код дружественной функции
+    cout << "Имя владельца: " << name.name << " Фамилия владельца: " << second_name.second_name<< " Возраст: "\
+         << age.age << " Марка авто: " << marka.marka << endl;
+}
+
+
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "RU");
     QCoreApplication a(argc, argv);
 
-    struct Time{
-        int Hours = 0;
-        int Minute = 0;
-        int Second = 0;
-    };
-/*
-    Building school;        //вот эти строки не соотвествуют принципу инкапсуляции
-    school.year = 1978;
-    school.type = "Школа";
-    school.get_info();
+    Person name("Рома", "Бычков", 25);
+    Car marka("BMW", 249);
 
-    Building house;
-    house.type = "Дом";
-    house.year = 2010;
-    house.get_info();*/
-
-
-   /*Building house;           //теперь из-за инициализации конструктора, который принимает значения, мы не можем создавать
-    house.set_data(2009, "Жилой дом");         //пустые объекты
-    house.get_info();
-
-    Building school;
-    school.set_data(1979, "Школа");
-    school.get_info();*/
-
-
-    Building school(2000, "Школа");
-    Building house(1991, "Жилой дом");
-    school.get_info();
-    house.get_info();
-    Building hospital(1982);
-
-    Rabotnik enjiener("Рома", 25, "Бычков");
-    enjiener.get_info();
-    enjiener.set_name("Вася");
-    enjiener.get_info();
-
+    print(name, name, name, marka);
 
     return 0;
 }
